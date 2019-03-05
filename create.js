0c176b6c11013e38ab777e720e2cd445d7052796
Ryan Agnello <ryanagnello213@gmail.com>
2019-03-05 10:49:48 -0500

    work

---
+++ create.js
@@ -0,0 +1,10 @@
+function start() {
+  var elem = document.getElementById('rip');
+      elem.parentNode.removeChild(elem);
+      return false;
+    yah();
+}
+
+function yah() {
+  var btn = document.createElement("IMG");
+}
