46c9009485767e0e425f22c98d578bfd0446fe4f
Ryan <ryanagnello213@gmail.com>
2019-03-06 18:51:34 -0500

    Work

--- create.js
+++ create.js
@@ -1,10 +1,20 @@
 function start() {
   var elem = document.getElementById('rip');
       elem.parentNode.removeChild(elem);
-      return false;
     yah();
 }
 
 function yah() {
-  var btn = document.createElement("IMG");
+  var img = document.createElement("IMG");
+  img.setAttribute("src", "https://i.amz.mshcdn.com/URoBdfh6G_-o8lNaH620WvL2YaM=/1200x627/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fstory%2Fthumbnail%2F21738%2F592857175_640.jpg");
+  img.setAttribute("id", "meme");
+  document.getElementById("image").appendChild(img);
+  buttons();
+}
+
+function buttons() {
+  var butt = document.createElement("input");
+  butt.type = "button";
+  butt.className = "opt";
+  document.getElementById("buttons").appendChild(butt);
 }
